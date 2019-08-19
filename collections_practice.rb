e0e8ca37138798fffc464d27d3f44ea01e580c20
Michael Lynch <michael@michaelrlynch.com>
2019-08-19 03:50:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,13 +20,6 @@ def reverse_array(array)
 end
 
 def kesha_maker(array)
-  i = 0
-
-  while i < array.length
-    yield(array[i])
-    i += 1
-  end
-  array[2] = "$"
-  array
-
+  array.each do |str|
+    str[2] = "$"
 end
