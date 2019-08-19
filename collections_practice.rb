be73cf7378a7b7711ebafb0d0639864489ddf78d
Michael Lynch <michael@michaelrlynch.com>
2019-08-19 05:42:34 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -37,7 +37,7 @@ end
 
 def add_s(array)
   array.each_with_index.collect do |word,index|
-        if index != 1 
+        if index != 1
           word = word + "s"
         else
           word = word
