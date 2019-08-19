2d10917451640ec59c5f56952043990c983d0307
Michael Lynch <michael@michaelrlynch.com>
2019-08-19 03:27:25 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,3 +9,7 @@ end
 def sort_array_char_count(array)
   array.sort_by { |char| char.length }
 end
+
+def swap_elements(array, index, destination_index)
+    array[0], array[2] = array[2], array[0]
+end
