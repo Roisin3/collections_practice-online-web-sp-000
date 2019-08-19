e6d8d423c21a37a390863c74938bb0a48dddd61d
Michael Lynch <michael@michaelrlynch.com>
2019-08-19 05:42:32 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -36,5 +36,11 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array[0, 2, 3] += "s"
+  array.each_with_index.collect do |word,index|
+        if index != 1 
+          word = word + "s"
+        else
+          word = word
+        end
+  end
 end
